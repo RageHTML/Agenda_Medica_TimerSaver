54e8becf183c6a6153049f7c88c08a5cfb836d89
RageHTML <deyvidm24@gmail.com>
2026-07-22 21:14:13 +0000

    feat() configurando a rota de cadastro para salvar no banco de dados, carregar os formularios para o html com jinja, calcular idade de acordo com o ano

--- AgendaMedica/app.py
+++ AgendaMedica/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, url_for, redirect
+from datetime import date
+from forms import RegistrationForm
+from models import db, User, Paciente, Medico, Consulta
 from dotenv import load_dotenv
 import os
 
@@ -10,15 +13,23 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "chave-padrao-seguranca")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///meubanco.db")
 
 
-@app.route("/login")
-def hello_world():
-    return render_template("register.html")
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        nasc = form.data_nascimento.data
+        idade = date.today().year - nasc.year
+
+        flash(f"Registro bem-sucedido para {form.nome.data}, idade: {idade} anos, email: {form.email.data}, CPF: {form.cpf.data}")
+        return redirect(url_for('agenda'))
+    
+    return render_template("register.html", form=form)
 
 @app.route("/save", methods=["POST"])
 def save():
     if request.method == "POST":
         return render_template("register.html", person=reg)
 
-@app.route("/login")
+@app.route("/agenda")
 def login():
-    return "<p>Login Page</p>"
+    return "<p>ja ja sai uma agenda</p>"
