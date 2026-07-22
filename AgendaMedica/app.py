02a359c5d9c57bdc987dfcff2b37d39fc2e2ce7f
RageHTML <deyvidm24@gmail.com>
2026-07-22 22:02:01 +0000

    feat() verificar se login existe no banco de dados, caso contrario mensagem de erro

--- AgendaMedica/app.py
+++ AgendaMedica/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template, flash, url_for, redirect
 from datetime import date
-from forms import RegistrationForm
+from forms import RegistrationForm, LoginForm
 from models import db, User, Paciente, Medico, Consulta
+from werkzeug.security import check_password_hash
 from dotenv import load_dotenv
 import os
 
@@ -21,14 +22,29 @@ def register():
         idade = date.today().year - nasc.year
 
         flash(f"Registro bem-sucedido para {form.nome.data}, idade: {idade} anos, email: {form.email.data}, CPF: {form.cpf.data}")
-        return redirect(url_for('agenda'))
+        return redirect(url_for('login'))
     
     return render_template("register.html", form=form)
 
-@app.route("/save", methods=["POST"])
-def save():
-    if request.method == "POST":
-        return render_template("register.html", person=reg)
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if not user:
+            flash("E-mail não encontrado.", "danger")
+            return render_template("login.html", form=form)
+
+        if not check_password_hash(user.senha, form.senha.data):
+            flash("Senha incorreta. Tente novamente.", "danger")
+            return render_template("login.html", form=form)
+
+        flash(f"Bem-vindo(a) de volta, {user.nome}!", "success")
+        return redirect(url_for("agenda"))
+
+    return render_template("login.html", form=form)
 
 @app.route("/agenda")
 def agenda():
